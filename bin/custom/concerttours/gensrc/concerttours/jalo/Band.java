c348f5c93f34e16cb0e5cf27a668c776f270c8d2
Davide Figuccia <d.figuccia@eu.aswatson.com>
2022-03-22 16:43:12 +0100

    Extend The Facade Layer

--- bin/custom/concerttours/gensrc/concerttours/jalo/Band.java
+++ bin/custom/concerttours/gensrc/concerttours/jalo/Band.java
@@ -1,7 +1,7 @@
 /*
  * ----------------------------------------------------------------
  * --- WARNING: THIS FILE IS GENERATED AND WILL BE OVERWRITTEN! ---
- * --- Generated at Mar 22, 2022, 3:32:37 PM                    ---
+ * --- Generated at Mar 22, 2022, 4:37:20 PM                    ---
  * ----------------------------------------------------------------
  */
 package concerttours.jalo;
