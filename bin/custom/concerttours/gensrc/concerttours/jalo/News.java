e8ea7631caedce2cba0421166c2eee1eab233de4
Davide Figuccia <d.figuccia@eu.aswatson.com>
2022-03-25 09:59:46 +0100

    Define a Custom Validation Constraint

--- bin/custom/concerttours/gensrc/concerttours/jalo/News.java
+++ bin/custom/concerttours/gensrc/concerttours/jalo/News.java
@@ -1,7 +1,7 @@
 /*
  * ----------------------------------------------------------------
  * --- WARNING: THIS FILE IS GENERATED AND WILL BE OVERWRITTEN! ---
- * --- Generated at Mar 25, 2022, 9:11:31 AM                    ---
+ * --- Generated at Mar 25, 2022, 9:49:32 AM                    ---
  * ----------------------------------------------------------------
  */
 package concerttours.jalo;
